dac3dacd3aea2f854d3ec519c8f12aa5894f6a85
Atlas484 <atlasaero484@gmail.com>
2020-08-22 12:41:26 -0500

    added tests for subtract, scale, dot, and cross for V3D

--- tests/Vector.test.ts
+++ tests/Vector.test.ts
@@ -88,6 +88,89 @@ describe('Vector3D', function () {
             expect(testVector.z).equal(result.z);
         });
     });
+    describe('subtract', function () {
+        it('should subtract the given Vector3D object to the vector', function () {
+            const testVector = new Vector3D(2, 3, 4);
+            const subVector = new Vector3D(3, 4, 5);
+            const result = new Vector3D(-1, -1, -1);
+
+            testVector.subtract(subVector);
+
+            expect(testVector.x).equal(result.x);
+            expect(testVector.y).equal(result.y);
+            expect(testVector.z).equal(result.z);;
+        });
+        it('should subtract the given Vector2D object to the vector', function () {
+            const testVector = new Vector3D(2, 3, 4);
+            const subVector = new Vector2D(3, 4);
+            const result = new Vector3D(-1, -1, 4);
+
+            testVector.subtract(subVector);
+
+            expect(testVector.x).equal(result.x);
+            expect(testVector.y).equal(result.y);
+            expect(testVector.z).equal(result.z);
+        });
+        it('should subtract the given 3 numbers to the vector', function () {
+            const testVector = new Vector3D(2, 3, 4);
+            const [x, y, z] = [1, 2, 3];
+            const result = new Vector3D(1, 1, 1);
+
+            testVector.subtract(x, y, z);
+
+            expect(testVector.x).equal(result.x);
+            expect(testVector.y).equal(result.y);
+            expect(testVector.z).equal(result.z);
+        });
+    });
+    describe('scale', function () {
+        it('should scale all of the vectors components by the given scalar', function () {
+            const testVector = new Vector3D(2, 3, 4);
+            const result = new Vector3D(10, 15, 20);
+
+            testVector.scale(5);
+
+            expect(testVector.x).equal(result.x);
+            expect(testVector.y).equal(result.y);
+            expect(testVector.z).equal(result.z);
+        });
+    });
+    describe('dot', function () {
+        it('should return the dot product of the vector and the given Vector3D objecy', function () {
+            const testVector = new Vector3D(1, 2, 3);
+            const dotVector = new Vector3D(4, 3, 2);
+            expect(testVector.dot(dotVector)).equal(16);
+        });
+        it('should return the dot product of the vector and the given Vector2D objecy', function () {
+            const testVector = new Vector3D(1, 2, 3);
+            const dotVector = new Vector2D(4, 3);
+            expect(testVector.dot(dotVector)).equal(10);
+        });
+    });
+    describe('cross', function () {
+        it('should return the cross product of the vector and the given Vector3D objecy', function () {
+            const testVector = new Vector3D(1, 0, 0);
+            const crossVector = new Vector3D(0, 1, 0);
+            const expectedResult = new Vector3D(0, 0, 1);
+
+            const result = testVector.cross(crossVector);
+
+            expect(expectedResult.x).equal(result.x);
+            expect(expectedResult.y).equal(result.y);
+            expect(expectedResult.z).equal(result.z);
+        });
+        it('should return the dot product of the vector and the given Vector2D objecy', function () {
+            const testVector = new Vector3D(1, 0, 0);
+            const crossVector = new Vector2D(0, 1);
+            const expectedResult = new Vector3D(0, 0, 1);
+
+            const result = testVector.cross(crossVector);
+
+            expect(expectedResult.x).equal(result.x);
+            expect(expectedResult.y).equal(result.y);
+            expect(expectedResult.z).equal(result.z);
+        });
+    });
 });
 
 // describe('Vector2D', function () {
